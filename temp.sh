51db869f377204d7f6bbda854f47c41142cc6488
Vincent Tavernier <vincent.tavernier@inria.fr>
2019-01-08 11:19:06 +0100

    Extract gnuplot script out of shell script

--- temp.sh
+++ temp.sh
@@ -2,15 +2,6 @@
 DEV=/dev/ttyACM0
 FILE=temp.csv
 
-echo >temp.gpi <<GNUPLOT
-set xdata time
-set timefmt '%Y-%m-%dT%H:%M:%S'
-set format x "%H:%M %d"
-set yrange [20:35]
-
-plot '$FILE' u 1:(\$2/10) w l
-GNUPLOT
-
 cat $DEV &>/dev/null
 socat stdio $DEV | tee -a $FILE &
 sleep 2
